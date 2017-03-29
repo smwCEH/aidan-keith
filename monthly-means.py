a1a550192d818b77fc6617e60b43e54fade18b9b
Simon Wright <smw@ceh.ac.uk>
2017-03-29 09:40:59 +0100

    Initial commit

---
+++ monthly-means.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+__author__ = 'smw'
+__email__ = 'smw@ceh.ac.uk'
+__status__ = 'Development'
+
+import os
+import sys
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
